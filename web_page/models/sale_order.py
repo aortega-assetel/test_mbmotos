6f025d4e577cb487ff1834a92792d8514fa411ed
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-04-23 10:44:54 -0500

    [ADD] model

---
+++ web_page/models/sale_order.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, fields
+import random
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    code = fields.Integer("Código")
+
+
+    @api.model
+    def create(self, vals):
+        result = super(SaleOrder, self).create(vals)
+        while True:
+            ran_number = random.randint(1000000000,9999999999)
+            repetido = reg.env['sale.order'].search_count([['code','=',ran_number]])
+            if repetido = 0:
+                vals['code'] = ran_number
+                break
+                
+
+        return result
+
