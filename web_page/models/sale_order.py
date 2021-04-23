228738c4d511bbf65a4471f5b468812623fa7a7f
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-04-23 10:56:19 -0500

    [fix] sale order create

--- web_page/models/sale_order.py
+++ web_page/models/sale_order.py
@@ -13,7 +13,7 @@ class SaleOrder(models.Model):
         result = super(SaleOrder, self).create(vals)
         while True:
             ran_number = random.randint(1000000000,9999999999)
-            repetido = reg.env['sale.order'].search_count([['code','=',ran_number]])
+            repetido = self.env['sale.order'].search_count([['code','=',ran_number]])
             if repetido == 0:
                 vals['code'] = ran_number
                 break
