e166385d80d355915a765c5906874139fb5cdb17
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-04-23 13:00:42 -0500

    [add] function to controller

--- web_page/controllers/controllers.py
+++ web_page/controllers/controllers.py
@@ -9,13 +9,39 @@ class PartnerPortal(http.Controller):
 
     @http.route(['/invoice/website/search/submit'], type='http', auth="public", website=True)
     def wisp_sign_in_form_submit(self, **post):
-        wisp_form = request.env['wisp.sign.in'].sudo().create({
-            'partner_name': post.get('partner_name'),
-            'email': post.get('email'),
-            'phone': post.get('phone')
-        })
-        vals = {
-            'wisp_form': wisp_form,
-        }
-        return request.render("wisp_sign_in.wisp_sign_in_website_form_success", vals)
+
+        order_id = self.env['sale.order'].search([['name','=',post.get('sale_number')],['code','=',post.get('code')]]).id
+
+        try:
+            order_sudo = self._document_check_access('sale.order', order_id, access_token=access_token)
+        except (AccessError, MissingError):
+            return request.redirect('/my')
+        
+        if report_type in ('html', 'pdf', 'text'):
+            return self._show_report(model=order_sudo, report_type=report_type, report_ref='sale.action_report_saleorder', download=download)
+
+        # use sudo to allow accessing/viewing orders for public user
+        # only if he knows the private token
+        # Log only once a day
+        if order_sudo:
+            # store the date as a string in the session to allow serialization
+            now = fields.Date.today().isoformat()
+            session_obj_date = request.session.get('view_quote_%s' % order_sudo.id)
+            if session_obj_date != now and request.env.user.share and access_token:
+                request.session['view_quote_%s' % order_sudo.id] = now
+                body = _('Quotation viewed by customer %s', order_sudo.partner_id.name)
+                _message_post_helper(
+                    "sale.order",
+                    order_sudo.id,
+                    body,
+                    token=order_sudo.access_token,
+                    message_type="notification",
+                    subtype_xmlid="mail.mt_note",
+                    partner_ids=order_sudo.user_id.sudo().partner_id.ids,
+                )
+
+        values = self._order_get_page_view_values(order_sudo, access_token, **kw)
+        values['message'] = message
+        
+        return request.render('sale.sale_order_portal_template', values)
         
