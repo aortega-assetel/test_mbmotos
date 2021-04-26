7987c6f77bb8baa30d60aea75c38e7f1cf68964e
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-04-26 13:15:47 -0500

    [fix] update manifest

--- web_page/__manifest__.py
+++ web_page/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale_management'],
+    'depends': ['base','sale_management','account_accountant','account','purchase','contacts'],
 
     # always loaded
     'data': [
