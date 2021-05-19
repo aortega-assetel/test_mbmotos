73daaa5ccffe9271413086294fded22eae5250a1
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-05-19 16:44:32 -0500

    [Add] accounting entry lines

--- assetel_manufacturing/models/mrp_production.py
+++ assetel_manufacturing/models/mrp_production.py
@@ -8,29 +8,30 @@ class MrpProduction(models.Model):
     def button_mark_done(self):
         result = super(MrpProduction, self).button_mark_done()
         if self.state == 'done':
-            for line in self.move_raw_ids:
-                if line.product_id.type == 'consu':
-                    if line.product_id.categ_id.property_valuation == 'real_time':
-                        move_lines = [
-                                    (0, 0, {
-                                        'account_id' : line.product_id.categ_id.property_stock_valuation_account_id.id,
-                                        'name': self.name +  ' - ' + line.product_id.name,
-                                        'credit': line.product_id.standard_price * line.quantity_done,
-                                    }),
-                                    (0, 0, {
-                                        'account_id' : line.product_id.property_stock_production.valuation_in_account_id.id,
-                                        'name': self.name +  ' - ' + line.product_id.name,
-                                        'debit': line.product_id.standard_price * line.quantity_done,
-                                    })
-                                ]
-                        values = {
-                            'ref' : self.name +  ' - ' + line.product_id.name,
-                            'date' : self.write_date,
-                            'journal_id' : line.product_id.categ_id.property_stock_journal.id,
-                            'line_ids' : move_lines
-                            }
-                        asiento = self.env['account.move'].create(values)
-                        asiento.action_post()
+            for line in self.workorder_ids:
+                
+                if line.product_id.categ_id.property_valuation == 'real_time':
+                    move_lines = [
+                                (0, 0, {
+                                    'account_id' : line.workcenter_id.cuenta_abono.id,
+                                    'name': self.name +  ' - ' + line.product_id.name,
+                                    'credit': line.duration * (line.workcenter_id.costs_hour/60),
+                                    
+                                }),
+                                (0, 0, {
+                                    'account_id' : line.workcenter_id.cuenta_cargo.id,
+                                    'name': self.name +  ' - ' + line.product_id.name,
+                                    'debit': line.duration * (line.workcenter_id.costs_hour/60),
+                                })
+                            ]
+                    values = {
+                        'ref' : self.name +  ' - ' + line.product_id.name,
+                        'date' : self.write_date,
+                        'journal_id' : line.product_id.categ_id.property_stock_journal.id,
+                        'line_ids' : move_lines
+                        }
+                    asiento = self.env['account.move'].create(values)
+                    asiento.action_post()
             
         return result
 
